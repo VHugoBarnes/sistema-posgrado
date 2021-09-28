1e67e5958591ee859d8341cb2124ecad349c28f8
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-09-28 10:34:20 -0500

    Rutas avance

--- app/Http/Controllers/PresentacionAvance.php
+++ app/Http/Controllers/PresentacionAvance.php
@@ -6,8 +6,26 @@ use Illuminate\Http\Request;
 
 class PresentacionAvance extends Controller
 {
-    //
+    // Vista
     public function index(){
         return view('avance.index');
+    }
+
+    // Vista
+    public function programarFecha(){
+        return view('avance.programar-fecha');
+    }
+
+    public function guardarFecha(){
+
+    }
+
+    // Vista
+    public function enviarReporte(){
+        return view('avance.enviar-reporte');
+    }
+
+    public function guardarReporte(){
+
     }
 }
--- routes/avance.php
+++ routes/avance.php
@@ -3,4 +3,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PresentacionAvance;
 
 Route::get('/presentacion-avance', [PresentacionAvance::class, 'index'])
+    ->middleware(['auth'])
     ->name('presentacion-avance');
+
+Route::get('/presentacion-avance/programar-fecha', [PresentacionAvance::class, 'programarFecha'])
+    ->middleware(['auth'])
+    ->name('presentacion-avance.programar-fecha');
+
+Route::post('/presentacion-avance/programar-fecha', [PresentacionAvance::class, 'guardarFecha'])
+    ->middleware(['auth'])
+    ->name('presentacion-avance.programar-fecha');
+
+Route::get('/presentacion-avance/enviar-reporte', [PresentacionAvance::class, 'enviarReporte'])
+    ->middleware(['auth'])
+    ->name('presentacion-avance.enviar-reporte');
+
+Route::post('/presentacion-avance/enviar-reporte', [PresentacionAvance::class, 'guardarReporte'])
+    ->middleware(['auth'])
+    ->name('presentacion-avance.enviar-reporte');
