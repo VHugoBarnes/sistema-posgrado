a46e2d5c10984977bbf3fad864edb80701341033
Paulina Michelle Gallegos Lopez <paulina_colem@hotmail.com>
2021-10-14 23:23:24 -0500

    Tabla arreglada

--- resources/views/egreso/revisar.blade.php
+++ resources/views/egreso/revisar.blade.php
@@ -9,7 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                @foreach ($tesis as $t)
+                <table  style="width: 100%">
+                            <style>
+                              table{
+                              border-collapse: collapse;
+                              border-spacing: 0;
+                              width: 100%;
+                              border: 1px solid #ddd;
+                              }
+                               th{
+                               padding: 1px;
+                               text-align: center;
+                              }
+                              td{
+                              padding: 1px;
+                              text-align: center;
+
+                              }
+                              
+                              </style>
+                                <tr>
+                                  <th style="width: 33%">Estudiante</th>
+                                  <th style="width: 33%">Número de control</th>
+                                  <th></th>
+                                  
+                                </tr>
+               @foreach ($tesis as $t) 
                     <div>
                
                       <!--  <h1 class="text-blue-400 p-2"><b>Estudiante:</b> {{ $t->estudiante->usuario->nombre }} {{ $t->estudiante->usuario->apellidos }}</h1>
@@ -52,12 +77,10 @@
                               }
                               
                               </style>
-                                <tr>
-                                  <th style="width: 33%">Estudiante</th>
-                                  <th style="width: 33%">Número de control</th>
-                                  
-                                </tr>
-                              
+                                <th style="width: 33%"></th>
+                                  <th style="width: 33%"></th>
+                                  <th></th>
+                
                                 <tr>
                                   <td>{{ $t->estudiante->usuario->nombre }} {{ $t->estudiante->usuario->apellidos }}</td>
                                   <td>{{ $t->estudiante->numero_control }}</td>
@@ -73,8 +96,8 @@
                             margin: 4px 2px;
                             opacity: 1;
                             transition: 0.3s;
-                            margin-top: 0%;
-                            margin-bottom: 6%;
+                            margin-top: 1%;
+                            margin-bottom: 1%;
                             }
 
                             .btn:hover {opacity: 0.6}
@@ -84,10 +107,11 @@
                             <button class="btn"><a href="{{ route('egresorevisardoc')}}"> Documentación</a></button>
                             </td>
                         </tr>
-  
+                          @endforeach
+                                
                           </table>
                     </div>
-                @endforeach
+              
                 </div>
             </div>
         </div>
